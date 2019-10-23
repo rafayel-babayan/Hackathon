f6afc932abf3e347aa6b577302d62782fca9003e
Rafayel <Rafayel@DESKTOP-3A2MHUU>
2019-10-23 12:42:49 +0400

    Commiit

--- Controllers/AdController.cs
+++ Controllers/AdController.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using reCAPTCHA.AspNetCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Hackathon.ViewModels;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace Hackathon.Controllers
 {
@@ -20,66 +23,55 @@ namespace Hackathon.Controllers
             _userRepository = userRepository;
         }
 
-        public async Task<IActionResult> Index() 
+
+        public async Task<IActionResult> Index()
         {
             return View();
         }
+
+
         [HttpGet]
         public IActionResult Create()
         {
-            ViewBag.Users = new SelectList(_userRepository.GetAllUsers(),"Id","Name"); 
+            ViewBag.Users = new SelectList(_userRepository.GetAllUsers(), "Id", "Name");
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Ad ad)
-        { 
+        public async Task<IActionResult> Create(AdViewModel ad)
+        {
             var recaptcha = await _rcService.Validate(Request);
+            Ad newAd=null;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                await ad.Image.CopyToAsync(stream);
+
+                if (stream.Length < 2097152)
+                {
+                    newAd = new Ad
+                    {
+                        Content = ad.Content,
+                        CreationDate = ad.CreationDate,
+                        Number = ad.Number,
+                        Rating = ad.Rating,
+                        UserId = ad.UserId,
+                        Image = stream.ToArray()
+                    };
+                }
+                else
+                {
+                    ModelState.AddModelError("Image", "The file is too large.");
+                }
+            }
 
             if (recaptcha.success && ModelState.IsValid)
             {
-                User user = new User { Name = "Rafo" };
-
-                Ad ads = new Ad
-                {
-                    Content = "Content",
-                    CreationDate = DateTime.Now,
-                    Number = 1,
-                    Rating = 10,
-                    User = user
-
-                };
-
-                _adRepository.SaveAd(ads);
-
-                
-            }else
-                return View();
-
-            return BadRequest();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Index(User usr)
-        {
-            var recaptcha = await _rcService.Validate(Request);
-
-            if (recaptcha.success)
+                _adRepository.SaveAd(newAd);
+                RedirectToAction(nameof(Create));
+            }
+            else
             {
-                User user = new User { Name = "Rafo" };
-
-                Ad ad = new Ad
-                {
-                    Content = "Content",
-                    CreationDate = DateTime.Now,
-                    Number = 1,
-                    Rating = 10,
-                    User = user
-
-                };
-
-                _adRepository.SaveAd(ad);
-                
+                ViewBag.Users = new SelectList(_userRepository.GetAllUsers(), "Id", "Name");
                 return View();
             }
 
--- ViewModels/AdViewModel.cs
+++ ViewModels/AdViewModel.cs
@@ -2,21 +2,19 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace Hackathon.Models
+namespace Hackathon.ViewModels
 {
-    public class Ad
+    public class AdViewModel
     {
-        public Guid Id { get; set; }
         [Required]
-        public int Number { get; set; }
-        public User User { get; set; }
-        public Guid UserId{ get; set; }
+        public int Number { get; set; }  
+        public Guid UserId { get; set; }
         [Required]
         public string Content { get; set; }
         [Required]
-        [Range(minimum:1,maximum:10)]
+        [Range(minimum: 1, maximum: 10)]
         public byte Rating { get; set; }
-        public byte[] Image { get; set; }
+        public IFormFile Image { get; set; }
         [Required]
         public DateTime CreationDate { get; set; }
     }
